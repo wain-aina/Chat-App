761ffbf134eb843bcbaf7081221c49e43f700f1b
wain-aina <murigipeninah1@gmail.com>
2024-06-18 09:20:21 +0300

    A Whats-App rip-off

---
+++ lib/widgets/new_message.dart
@@ -0,0 +1,79 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget{
+  const NewMessage({super.key});
+
+  @override
+  State<NewMessage> createState(){
+    return _NewMessageState();
+  }
+}
+
+class _NewMessageState extends State<NewMessage>{
+
+  final _messageController = TextEditingController();
+
+  @override
+  void dispose(){
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void _submitMessage() async {
+    final enteredMessage = _messageController.text;
+    if(enteredMessage.trim().isEmpty){
+      return;
+    }
+
+     FocusScope.of(context).unfocus();
+     _messageController.clear();
+
+    final user = FirebaseAuth.instance.currentUser!;
+
+    final user_data = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+
+    FirebaseFirestore
+      .instance
+      .collection('chat')
+      .add({
+        'text': enteredMessage,
+        'createdAt': Timestamp.now(),
+        'userId': user.uid,
+        'username': user_data.data()!['username'],
+        'userImage': user_data.data()!['image_url']
+      });
+   
+  }
+
+  Widget build(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 15,
+        right: 1, 
+        bottom: 14
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              textCapitalization: TextCapitalization.sentences,
+              autocorrect: true,
+              enableSuggestions: true,
+              decoration: InputDecoration(labelText: 'Send A Message......'),
+            )
+          ),
+          IconButton(
+            color: Theme.of(context).colorScheme.primary,
+            onPressed: _submitMessage, 
+            icon: const Icon(
+              Icons.send
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
